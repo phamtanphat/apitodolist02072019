9229729c1c09a71ff52bc732ebc59be12caf8ff3
phatdroid94 <phatdroid94@gmail.com>
2019-10-01 17:52:44 +0700

    a

--- register.php
+++ register.php
@@ -6,4 +6,11 @@
     
     $insertAccount = "INSERT INTO user VALUES (null , $username , $password)";
 
+    $data = mysqli_query($con , $insertAccount);
+
+    if($data){
+        echo "Thanh cong";
+    }else{
+        echo "That bai";
+    }
 ?>
